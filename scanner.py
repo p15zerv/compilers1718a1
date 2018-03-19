92adad64fff294e7dece11422721b4edc2e18cd7
Alex Zervopoulos <p15zerv@ionio.gr>
2018-03-19 03:23:40 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -9,19 +9,19 @@ def getchar(words, pos):
     c = words[pos]
 
     if c in '01':
-        return 'DIGIT_UPTO1'
+        return 'D_UPTO1'
 
     if c == '2':
-        return 'DIGIT_2'
+        return 'D_2'
 
     if c == '3':
-        return 'DIGIT_3'
+        return 'D_3'
 
     if c in '45':
-        return 'DIGIT_UPTO5'
+        return 'D_UPTO5'
 
     if c in '6789':
-        return 'ANY_DIGIT'
+        return 'D_ANY'
 
     if c in ':.':
         return 'DELIMITER'
@@ -58,27 +58,19 @@ def scan(text, transition_table, accept_states):
             return 'ERROR_TOKEN', pos
 
 
-def groups_up_to(group):
-    """ Returns a list of the groups up to and including `group`
-    """
-    digit_group_priority = ['DIGIT_UPTO1', 'DIGIT_2', 'DIGIT_3',
-                            'DIGIT_UPTO5', 'ANY_DIGIT']
-    return [g for g in digit_group_priority[:digit_group_priority.index(group) + 1]]
-
-
 # the transition table, as a dictionary
-td = {'q0': {'DIGIT_UPTO1': 'q1', 'DIGIT_2': 'q6', 'DIGIT_UPTO5': 'q2', 'ANY_DIGIT': 'q2'},
-      # using comprehensions to make the transition table shorter and hopefully more readable
-      'q1': {g: 'q2' for g in groups_up_to('ANY_DIGIT')},
-      'q2': {'DELIMITER': 'q3'},
-      'q3': {g: 'q4' for g in groups_up_to('DIGIT_UPTO5')},
-      'q4': {g: 'q5' for g in groups_up_to('ANY_DIGIT')},
-      'q6': {g: 'q2' for g in groups_up_to('DIGIT_3')}
-      }
+td = {
+    'q0': {'D_UPTO1': 'q1', 'D_2': 'q3', 'D_3': 'q2', 'D_UPTO5': 'q2', 'D_ANY': 'q2'},
+    'q1': {'D_UPTO1': 'q2', 'D_2': 'q2', 'D_3': 'q2', 'D_UPTO5': 'q2', 'D_ANY': 'q2', 'DELIMITER': 'q4'},
+    'q2': {'DELIMITER': 'q4'},
+    'q3': {'D_UPTO1': 'q2', 'D_2': 'q2', 'D_3': 'q2', 'DELIMITER': 'q4'},
+    'q4': {'D_UPTO1': 'q5', 'D_2': 'q5', 'D_3': 'q5', 'D_UPTO5': 'q5'},
+    'q5': {'D_UPTO1': 'q6', 'D_2': 'q6', 'D_3': 'q6', 'D_UPTO5': 'q6', 'D_ANY': 'q6'}
+}
 
 # the dictionary of accepting states and their
 # corresponding token
-ad = {'q5': 'TIMESTAMP_TOKEN'}
+ad = {'q6': 'TIME_TOKEN'}
 
 # get a string from input
 text = input('give some input> ')
